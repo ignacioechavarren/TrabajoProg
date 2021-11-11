01eeb08f3884a92e49e636fa041e766efe69d676
ina <ina@10.164.49.170>
2021-11-11 17:03:25 +0100

    Empezando poniendo panel

---
+++ TrabajoProg/src/VentanaInicio.java
@@ -0,0 +1,9 @@
+
+public class VentanaInicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- TrabajoProg/src/VentanaRegistrar.java
+++ TrabajoProg/src/VentanaRegistrar.java
@@ -1,5 +1,5 @@
 
-public class TrabajoProg {
+public class VentanaRegistrar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
