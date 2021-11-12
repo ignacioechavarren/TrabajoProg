17f8b0c733bf69b88e15bc9f366ab01c8d25b5f7
ignacioechavarren <ignacioechavarren@opendeusto.es>
2021-11-12 17:20:29 +0100

    Primeros parámetros de la ventana

--- TrabajoProg/src/VentanaLogin.java
+++ TrabajoProg/src/VentanaLogin.java
@@ -1,13 +1,32 @@
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
 
 public class VentanaLogin extends JFrame {
 	
+	private Image img_user = new ImageIcon(VentanaLogin.class.getResource("imagenes/gente.png")).getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+	private Image img_lock = new ImageIcon(VentanaLogin.class.getResource("imagenes/candado.png")).getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+	private Image img_eye = new ImageIcon(VentanaLogin.class.getResource("imagenes/eye.png")).getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+	
+	//las imagenes que vamos a utilizar
+	
+	
 	private JPanel contentPane;
+	private JTextField txtUsuario;
+	private JPasswordField txtPass;
 	
 
 
@@ -28,7 +47,7 @@ public class VentanaLogin extends JFrame {
 	public VentanaLogin() {
 		setUndecorated(true);
 		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);                     //los parametros de la ventana
 		setBounds(100,100,450,300);
 		setSize(800,600);
 		setLocationRelativeTo(null);
@@ -38,6 +57,77 @@ public class VentanaLogin extends JFrame {
 		contentPane.setBorder(new LineBorder(new Color(0,0,0,0)));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JLabel lblPanel = new JLabel("panel");
+		lblPanel.setForeground(Color.WHITE);
+		lblPanel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPanel.setBounds(0, 0, 375, 600);
+		lblPanel.setIcon(new ImageIcon(VentanaLogin.class.getResource("/imagenes/panel.png")));
+		lblPanel.setSize(375,600);
+		contentPane.add(lblPanel);
+		
+		JLabel lblTitule = new JLabel("INICIAR SESI\u00D3N");
+		lblTitule.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		lblTitule.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitule.setFont(new Font("Arial Black", Font.PLAIN, 30));
+		lblTitule.setBounds(425, 89, 328, 51);
+		contentPane.add(lblTitule);
+		
+		JLabel lblUser = new JLabel("USUARIO");
+		lblUser.setFont(new Font("Arial Black", Font.PLAIN, 17));
+		lblUser.setBounds(454, 220, 101, 30);
+		contentPane.add(lblUser);
+		
+		JLabel lblContraseña = new JLabel("CONTRASE\u00D1A");
+		lblContraseña.setForeground(Color.BLACK);
+		lblContraseña.setBackground(Color.WHITE);
+		lblContraseña.setFont(new Font("Arial Black", Font.PLAIN, 17));
+		lblContraseña.setBounds(454, 310, 142, 30);
+		contentPane.add(lblContraseña);
+		
+		txtUsuario.setBorder(null);
+		txtUsuario.setToolTipText("");
+		txtUsuario.setBackground(Color.WHITE);
+		txtUsuario.setFont(new Font("Arial Black", Font.PLAIN, 13));
+		txtUsuario.setForeground(Color.LIGHT_GRAY);
+		txtUsuario.setText("Ingrese su nombre de usuario");
+		txtUsuario.setBounds(454, 257, 270, 30);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		txtPass = new JPasswordField();
+		txtPass.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) { // cuando sea presionado, el texto debe desaparecer
+				txtUsuario.setText("Ingrese su nombre de usuario");
+				txtPass.setText("");
+				txtPass.setForeground(Color.BLACK); // despues de haber sido presionado, cambie la letra de color
+
+			}
+		});
+		txtPass.setBorder(null);
+		txtPass.setText("********");
+		txtPass.setSelectedTextColor(Color.WHITE);
+		txtPass.setToolTipText("");
+		txtPass.setFont(new Font("Arial Black", Font.PLAIN, 13));
+		txtPass.setHorizontalAlignment(SwingConstants.LEFT);
+		txtPass.setBackground(Color.WHITE);
+		txtPass.setForeground(Color.LIGHT_GRAY);
+		txtPass.setBounds(454, 350, 270, 30);
+		contentPane.add(txtPass);
+		
+		JPanel btnEntrar = new JPanel();
+		btnEntrar.setForeground(new Color(153, 204, 204));
+		btnEntrar.setBackground(new Color(153, 204, 204));
+		btnEntrar.setBounds(568, 425, 156, 51);
+		contentPane.add(btnEntrar);
+		btnEntrar.setLayout(null);
+		
+		JLabel lblEntrar = new JLabel("ENTRAR");
+		lblEntrar.setBounds(0, 0, 156, 51);
+		btnEntrar.add(lblEntrar);
+		lblEntrar.setBorder(new LineBorder(new Color(0, 0, 0)));
+		
 		
 		
 		
--- TrabajoProg/src/VentanaRegistrar.java
+++ TrabajoProg/src/VentanaRegistrar.java
@@ -1,9 +1,182 @@
 
+<<<<<<< HEAD
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+
+public class VentanaRegistrar extends JFrame {
+	
+	private JPanel contentPane;
+	int xMouse, yMouse; // nos servira para el panel_move
+
+
+	public static void main(String[] args) {
+		
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaRegistrar frame = new VentanaRegistrar();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+	}
+	
+	public VentanaRegistrar() {
+		
+		setUndecorated(true); // quitaremos los comandos para diseñarlos y localizarlos a nuestro gusto
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setSize(600, 700);
+		setLocationRelativeTo(null); // la ventana aparecera en el centro de la pantalla
+		
+		// 00- panel principal
+		
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(153, 204, 204));
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+//		01- este sera un panel que nos permitira mover la ventana, siguiendo al mouse
+		
+		JPanel panel_move = new JPanel(); 
+		panel_move.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				setLocation(x - xMouse, y - yMouse); // con esto me permite mover la pagina a mi gusto
+			}
+		});
+		panel_move.addMouseListener(new MouseAdapter() { 
+			@Override
+			public void mousePressed(MouseEvent e) { // localizara al mouse 
+				xMouse = e.getX();
+				yMouse = e.getY();
+			}
+		});
+		panel_move.setBorder(null);
+		panel_move.setBackground(new Color(153, 204, 204));
+		panel_move.setBounds(0, 0, 537, 51);
+		contentPane.add(panel_move);
+		
+//		02- Boton que nos permitira salir de las ventanas
+		// Creamos un panel para despues agregar color a la hora de que el mouse se acerque
+		
+		JPanel btnSalir = new JPanel();
+		btnSalir.setBackground(new Color(153, 204, 204));
+		btnSalir.setBounds(540, 0, 60, 58);
+		contentPane.add(btnSalir);
+		btnSalir.setLayout(null);
+		
+		// Un label donde indicaremos la accion "X"
+		
+		JLabel lblSalir = new JLabel("X");
+		lblSalir.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) { // cuando sea clicado, saldremos
+				System.exit(0);
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) { // si el mouse entra, cambiara de color
+				btnSalir.setBackground(Color.RED);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) { // si el mouse sale, volvera al color inicial
+				btnSalir.setBackground(new Color(153, 204, 204));
+			}
+		});
+		lblSalir.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // cuando el mouse sobre el label, se mostrara una mano de precionado
+		lblSalir.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSalir.setFont(new Font("Arial", Font.PLAIN, 30));
+		lblSalir.setBounds(0, 0, 60, 58);
+		btnSalir.add(lblSalir);
+		
+//		09- Boton de creado de cuenta: queremos que una vez precionado el boton, se guarden los datos del usuario y salta a la ventanaInicio
+		// Creamos un panel para despues agregar color a la hora de que el mouse se acerque
+		
+		JPanel btnCrear = new JPanel();
+		btnCrear.setBounds(139, 522, 328, 42);
+		contentPane.add(btnCrear);
+		btnCrear.setLayout(null);
+		
+		// un label donde indicaremos la accion "crear cuenta"
+		
+		JLabel lblCrear = new JLabel("Crear cuenta");
+		lblCrear.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // para que aparezca una mano en vez de una flecha
+		lblCrear.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) { // si el mouse es clicado, saltaremos a VentanaInicio
+				VentanaInicio newframe = new VentanaInicio();
+//				newframe.setVisible(true);
+				dispose();
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) { // si el mouse entra, cambiara de color
+				btnCrear.setBackground(new Color( 0, 153, 153));
+				lblCrear.setForeground(Color.WHITE);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) { // si el mouse sale, vuelve a su color inicial
+				btnCrear.setBackground(Color.WHITE);
+				lblCrear.setForeground(Color.BLACK);
+			}
+		});
+		lblCrear.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCrear.setFont(new Font("Arial Black", Font.PLAIN, 20));
+		lblCrear.setBounds(0, 0, 328, 42);
+		btnCrear.add(lblCrear);
+		
+//		10- Boton incia sesion: boton que nos devolvera a la ventana iniciar sesion
+		// un label donde indicaremos la accion "iniciar sesion"
+
+		JLabel lblInicia = new JLabel("Inicia sesi\u00F3n");
+		lblInicia.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // para que aparezca una mano en vez de una flecha
+		lblInicia.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) { // si el mouse es clicado, volveremos a VentanaLogin
+				VentanaLogin newframe = new VentanaLogin();
+				newframe.setVisible(true);
+				dispose();
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) { // si el mouse entra, cambiara de color
+				lblInicia.setForeground(new Color( 0, 153, 153));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) { // si el mouse sale, vuelve a su color inicial
+				lblInicia.setForeground(new Color(102, 102, 102));
+			}
+		});
+		lblInicia.setForeground(new Color(102, 102, 102));
+		lblInicia.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInicia.setFont(new Font("Arial Black", Font.PLAIN, 17));
+		lblInicia.setBounds(200, 578, 200, 30);
+		contentPane.add(lblInicia);
+		
+=======
+
 public class VentanaRegistrar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+>>>>>>> branch 'master' of https://github.com/ignacioechavarren/TrabajoProg.git
 	}
 
 }
