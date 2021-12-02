4ba2eba8b0a5bc398d0dccac0ae3ec7bb2b52d7e
joelg <joelg@192.168.0.17>
2021-12-02 17:18:36 +0100

    Problema de inicio arreglado

--- TrabajoProg/src/VentanaRegistrar.java
+++ TrabajoProg/src/VentanaRegistrar.java
@@ -19,7 +19,7 @@ import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 
 public class VentanaRegistrar extends JFrame {
-		
+
 	private JPanel contentPane;
 	int xMouse, yMouse; // nos servira para el panel_move
 	private JTextField textNom;
@@ -39,28 +39,28 @@ public class VentanaRegistrar extends JFrame {
 				}
 			}
 		});
-		
+
 	}
-	
+
 	public VentanaRegistrar() {
-		
+
 		setUndecorated(true); // quitaremos los comandos para diseñarlos y localizarlos a nuestro gusto
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		setSize(600, 700);
 		setLocationRelativeTo(null); // la ventana aparecera en el centro de la pantalla
-		
+
 //		00- panel principal
-		
+
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(153, 204, 204));
 		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 //		01- este sera un panel que nos permitira mover la ventana, siguiendo al mouse
-		
-		JPanel panel_move = new JPanel(); 
+
+		JPanel panel_move = new JPanel();
 		panel_move.addMouseMotionListener(new MouseMotionAdapter() {
 			@Override
 			public void mouseDragged(MouseEvent e) { // localizara al mouse cuando sea arrastrado
@@ -69,7 +69,7 @@ public class VentanaRegistrar extends JFrame {
 				setLocation(x - xMouse, y - yMouse);
 			}
 		});
-		panel_move.addMouseListener(new MouseAdapter() { 
+		panel_move.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) { // localizara al mouse cuando sea presionado
 				xMouse = e.getX();
@@ -80,41 +80,45 @@ public class VentanaRegistrar extends JFrame {
 		panel_move.setBackground(new Color(153, 204, 204));
 		panel_move.setBounds(0, 0, 537, 51);
 		contentPane.add(panel_move);
-		
+
 //		02- Boton que nos permitira salir de las ventanas
-		// Creamos un panel para despues agregar color a la hora de que el mouse se acerque
-		
+		// Creamos un panel para despues agregar color a la hora de que el mouse se
+		// acerque
+
 		JPanel btnSalir = new JPanel();
 		btnSalir.setBackground(new Color(153, 204, 204));
 		btnSalir.setBounds(540, 0, 60, 58);
 		contentPane.add(btnSalir);
 		btnSalir.setLayout(null);
-		
+
 		// Un label donde indicaremos la accion "X"
-		
+
 		JLabel lblSalir = new JLabel("X");
 		lblSalir.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) { // cuando sea clicado, saldremos
 				System.exit(0);
 			}
+
 			@Override
 			public void mouseEntered(MouseEvent e) { // si el mouse entra, cambiara de color
 				btnSalir.setBackground(Color.RED);
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) { // si el mouse sale, volvera al color inicial
 				btnSalir.setBackground(new Color(153, 204, 204));
 			}
 		});
-		lblSalir.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // cuando el mouse sobre el label, se mostrara una mano de precionado
+		lblSalir.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // cuando el mouse sobre el label, se
+																			// mostrara una mano de precionado
 		lblSalir.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSalir.setFont(new Font("Arial", Font.PLAIN, 30));
 		lblSalir.setBounds(0, 0, 60, 58);
 		btnSalir.add(lblSalir);
-		
+
 //		03- titulo
-		
+
 		JLabel lblTitle = new JLabel("REGISTRATE");
 		lblTitle.setBackground(Color.WHITE);
 		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
@@ -122,41 +126,41 @@ public class VentanaRegistrar extends JFrame {
 		lblTitle.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
 		lblTitle.setBounds(139, 53, 328, 51);
 		contentPane.add(lblTitle);
-		
+
 //		04- usuario: solicitaremos un nombre de usuario
-		
+
 		JLabel lblNombreYApellidos = new JLabel("Nombre de usuario:");
 		lblNombreYApellidos.setForeground(new Color(102, 102, 102));
 		lblNombreYApellidos.setFont(new Font("Arial Black", Font.PLAIN, 17));
 		lblNombreYApellidos.setBounds(139, 139, 200, 30);
 		contentPane.add(lblNombreYApellidos);
-		
+
 //		05- correo: solicitaremos un correo
-		
+
 		JLabel lblCorreo = new JLabel("Direcci\u00F3n de email:");
 		lblCorreo.setForeground(new Color(102, 102, 102));
 		lblCorreo.setFont(new Font("Arial Black", Font.PLAIN, 17));
 		lblCorreo.setBounds(139, 220, 200, 30);
 		contentPane.add(lblCorreo);
-		
+
 //		06- contraseña: solicitaremos una contraseña
-		
+
 		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
 		lblContrasea.setForeground(new Color(102, 102, 102));
 		lblContrasea.setFont(new Font("Arial Black", Font.PLAIN, 17));
 		lblContrasea.setBounds(139, 300, 200, 30);
 		contentPane.add(lblContrasea);
-		
+
 //		07- confirmacion: solicitaremos la comfirmacion de la contraseña
-		
+
 		JLabel lblConfirmacin = new JLabel("Confirma tu contrase\u00F1a:");
 		lblConfirmacin.setForeground(new Color(102, 102, 102));
 		lblConfirmacin.setFont(new Font("Arial Black", Font.PLAIN, 17));
 		lblConfirmacin.setBounds(139, 380, 240, 30);
 		contentPane.add(lblConfirmacin);
-		
+
 //		08- nombre: apartado donde el usuario escribira su nombre
-		
+
 		textNom = new JTextField();
 		textNom.setToolTipText("");
 		textNom.setForeground(Color.BLACK);
@@ -166,9 +170,9 @@ public class VentanaRegistrar extends JFrame {
 		textNom.setBackground(Color.WHITE);
 		textNom.setBounds(139, 180, 328, 30);
 		contentPane.add(textNom);
-		
+
 //		09- email: apartado donde el usuario escribira su email
-		
+
 		textEmail = new JTextField();
 		textEmail.setToolTipText("");
 		textEmail.setForeground(Color.BLACK);
@@ -178,9 +182,9 @@ public class VentanaRegistrar extends JFrame {
 		textEmail.setBackground(Color.WHITE);
 		textEmail.setBounds(139, 260, 328, 30);
 		contentPane.add(textEmail);
-		
+
 //		10- contraseña: apartado donde el usuario escribira su contraseña
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setToolTipText("");
 		passwordField.setSelectedTextColor(Color.WHITE);
@@ -191,9 +195,9 @@ public class VentanaRegistrar extends JFrame {
 		passwordField.setBackground(Color.WHITE);
 		passwordField.setBounds(139, 340, 270, 30);
 		contentPane.add(passwordField);
-		
+
 //		11- confirmar: apartado donde el usuario confirmara su contraseña
-		
+
 		passwordField_1 = new JPasswordField();
 		passwordField_1.setToolTipText("");
 		passwordField_1.setSelectedTextColor(Color.WHITE);
@@ -204,30 +208,34 @@ public class VentanaRegistrar extends JFrame {
 		passwordField_1.setBackground(Color.WHITE);
 		passwordField_1.setBounds(139, 420, 270, 30);
 		contentPane.add(passwordField_1);
-		
+
 //		12- Boton de creado de cuenta: queremos que una vez precionado el boton, se guarden los datos del usuario y salta a la ventanaInicio
-		// Creamos un panel para despues agregar color a la hora de que el mouse se acerque
-		
+		// Creamos un panel para despues agregar color a la hora de que el mouse se
+		// acerque
+
 		JPanel btnCrear = new JPanel();
 		btnCrear.setBounds(139, 522, 328, 42);
 		contentPane.add(btnCrear);
 		btnCrear.setLayout(null);
-		
+
 		// un label donde indicaremos la accion "crear cuenta"
-		
+
 		JLabel lblCrear = new JLabel("Crear cuenta");
-		lblCrear.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // para que aparezca una mano en vez de una flecha
+		lblCrear.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // para que aparezca una mano en vez de una
+																			// flecha
 		lblCrear.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) { // si el mouse es clicado, saltaremos a VentanaInicio
 				VentanaInicio newframe = new VentanaInicio();
-			//	newframe.setVisible(true);
-		
+				newframe.setVisible(true);
+			}
+		});
 //		13- Boton incia sesion: boton que nos devolvera a la ventana iniciar sesion
 		// un label donde indicaremos la accion "iniciar sesion"
 
 		JLabel lblInicia = new JLabel("Inicia sesion");
-		lblInicia.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // para que aparezca una mano en vez de una flecha
+		lblInicia.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)); // para que aparezca una mano en vez de una
+																				// flecha
 		lblInicia.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) { // si el mouse es clicado, volveremos a VentanaLogin
@@ -235,10 +243,12 @@ public class VentanaRegistrar extends JFrame {
 				newframe.setVisible(true);
 				dispose();
 			}
+
 			@Override
 			public void mouseEntered(MouseEvent e) { // si el mouse entra, cambiara de color
-				lblInicia.setForeground(new Color( 0, 153, 153));
+				lblInicia.setForeground(new Color(0, 153, 153));
 			}
+
 			@Override
 			public void mouseExited(MouseEvent e) { // si el mouse sale, vuelve a su color inicial
 				lblInicia.setForeground(new Color(102, 102, 102));
@@ -249,6 +259,5 @@ public class VentanaRegistrar extends JFrame {
 		lblInicia.setFont(new Font("Arial Black", Font.PLAIN, 17));
 		lblInicia.setBounds(200, 578, 200, 30);
 		contentPane.add(lblInicia);
-
-}}
-		
+	}
+}
